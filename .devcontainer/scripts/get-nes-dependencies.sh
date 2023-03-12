933e760d63e466c2f75cda66ffe0f0832de0fdc2
Nick V <NickVanderPyle@users.noreply.github.com>
2023-03-12 16:42:20 -0400

    Init commit

---
+++ .devcontainer/scripts/get-nes-dependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+path_to_workspace="$(dirname $0)/../.."
+
+curl --silent -L https://github.com/cc65/cc65/archive/refs/heads/master.zip -o /tmp/cc65.zip
+
+unzip -q /tmp/cc65.zip -d /tmp/cc65/
+
+mkdir -p "${path_to_workspace}/include"
+mv -n /tmp/cc65/cc65-master/asminc/nes.inc "${path_to_workspace}/include"
+
+rm -rf /tmp/cc65
